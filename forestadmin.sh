1073390d7dead4911eb4ffa0f5d34bb580a28182
Manuel <manuel.c1992@gmail.com>
2019-02-11 15:15:01 -0500

    Add master script

---
+++ forestadmin.sh
@@ -0,0 +1,12 @@
+node /home/vrm/forestadmin_csv/getITC
+node /home/vrm/forestadmin_csv/getITCenhanced
+node /home/vrm/forestadmin_csv/uploadBruno
+node /home/vrm/forestadmin_csv/uploadBruno4
+node /home/vrm/forestadmin_csv/uploadBruno6
+node /home/vrm/forestadmin_csv/uploadITC-ID
+node /home/vrm/forestadmin_csv/uploadJesus
+node /home/vrm/forestadmin_csv/uploadOyon
+node /home/vrm/forestadmin_csv/uploadSam2
+node /home/vrm/forestadmin_csv/uploadSam3
+node /home/vrm/forestadmin_csv/uploadSam4
+node /home/vrm/forestadmin_csv/uploadSkyler
