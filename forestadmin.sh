a3bd5119de39df9711c31976e5cc2e3c982849d7
Manuel <manuel.c1992@gmail.com>
2019-07-10 21:05:31 -0500

    Delete old scripts

--- forestadmin.sh
+++ forestadmin.sh
@@ -1,11 +1,7 @@
 node /home/zeno/forestadmin_csv/getITC
-node /home/zeno/forestadmin_csv/getITCenhanced
-node /home/zeno/forestadmin_csv/uploadBruno
 node /home/zeno/forestadmin_csv/uploadBruno4
 node /home/zeno/forestadmin_csv/uploadBruno6
 node /home/zeno/forestadmin_csv/uploadITC-ID
 node /home/zeno/forestadmin_csv/uploadOyon
-node /home/zeno/forestadmin_csv/uploadSam2
 node /home/zeno/forestadmin_csv/uploadSam3
-node /home/zeno/forestadmin_csv/uploadSam4
 node /home/zeno/forestadmin_csv/uploadSkyler
